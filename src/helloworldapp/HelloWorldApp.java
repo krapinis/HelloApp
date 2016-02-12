b698269078b08880f122f47f60ef5280e85973e8
Mantas Zinkevicius <mantas1zinkevicius@gmail.com>
2016-02-12 17:11:24 +0100

    netbeans with problems

--- src/helloworldapp/HelloWorldApp.java
+++ src/helloworldapp/HelloWorldApp.java
@@ -5,6 +5,10 @@
  */
 package helloworldapp;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
 /**
  *
  * @author Mantas
@@ -16,8 +20,26 @@ public class HelloWorldApp {
      */
 public static void main(String[] args) {
     HelloWorldApp obj = new HelloWorldApp();
-    System.out.println("");
-            
-            
+    System.out.println(obj.getFile("text.txt"));
 }    
+
+private String getFile(String fileName){
+    StringBuilder result = new StringBuilder("");
+    ClassLoader classLoader = getClass().getClassLoader();
+    File file = new File(classLoader.getResource(fileName).getFile());
+    
+    try (Scanner scanner = new Scanner(file)){
+        
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            result.append(line).append("/n");
+            
+        }
+        scanner.close();
+    }   catch (IOException e){
+        e.printStackTrace();
+    }
+    return result.toString();
+}
+
 }
